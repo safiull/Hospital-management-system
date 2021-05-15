b5ace9fca015589d484b95f2ba7bb8bcc5c18c58
safiull <safiullalam9931@gmail.com>
2021-05-15 23:29:18 +0600

    resolve

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\xampp\htdocs\hospital\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xampp\htdocs\hospital\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\xampp\htdocs\Hospital-management-system\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\xampp\htdocs\Hospital-management-system\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
